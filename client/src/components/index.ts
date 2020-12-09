86ec778602ad07c025e90b9eb0e057edfe3bd95d
isaquetdiniz <itd@cin.ufpe.br>
2020-12-09 11:13:43 -0300

    (deploy-server): deleted client

--- client/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import InputSearch from "./InputSearch";
-import TableComponent from "./Table";
-import InputForm from "./InputForm";
-
-export {
-    InputSearch,
-    TableComponent,
-    InputForm
-}
